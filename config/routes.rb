625ddec6a5999335de05c4cf0d6cffa3ccbbf15d
Vincent <xrocxion@gmail.com>
2017-09-20 21:12:52 -0700

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home', to: 'home#index'
+  get 'about', to: 'home#about'
+  get 'events', to: 'home#events'
+  get 'resources', to: 'home#resources'
+  get 'calendar', to: 'home#calendar'
+  get 'contact-us', to: 'home#contact-us'
+
+  get 'error', to: 'home#error'
 
   resources :articles do
   	resources :comments
